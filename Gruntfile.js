f17d78a778d2076e0e2e13ed85d79f8bfc0f2ee4
Troy Whiteley <troy@somany.us>
2014-02-27 18:46:33 -0800

    First pass at mobile support.
    Fixes #24

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadTasks('tasks');
-  grunt.registerTask('script-dist', ['concat:dist', 'uglify:dist']);
-  grunt.registerTask('script-full', ['concat:full', 'uglify:full']);
+  grunt.registerTask('script-dist', ['concat:dist', 'concat:distMobile', 'uglify:dist', 'uglify:distMobile']);
+  grunt.registerTask('script-full', ['concat:full', 'concat:fullMobile', 'uglify:full', 'uglify:fullMobile']);
   grunt.registerTask('scripts', ['jshint', 'bower', 'script-dist', 'script-full', 'template2js', 'clean:bower', 'mocha', 'bytesize', 'notify:generic']);
-  grunt.registerTask('default', ['scripts']);
+  grunt.registerTask('default', ['scripts', 'less']);
   grunt.registerTask('dev', ['default', 'connect:server', 'notify:watch', 'watch']);
 };
